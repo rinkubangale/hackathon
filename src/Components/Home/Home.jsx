9d47ead32c8ae10e0ef7cc21fd420827a959665d
rinkubangale <bangalehemant@gmail.com>
2021-11-20 10:34:18 +0530

    homePage

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Styles from './Home.module.css';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+
+export default function BasicTextFields() {
+  const defaultData = {
+    to: "",
+    from: "",
+    date: new Date('2021-11-20')
+  }
+
+  const [data, setData] = React.useState(defaultData);
+
+  const handleDate = (e) => {
+    setData({...data, "date":e});
+  }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+  
+  return (
+    <Box className={Styles.homeBox}
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="From where?" variant="outlined" name="from" onChange={handleChange}  />
+      <TextField id="outlined-basic" label="To where?" variant="outlined" name="to" onChange={handleChange}  />
+     <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DesktopDatePicker
+          label="When ?"
+          inputFormat="dd/MM/yyyy"
+          value={data.date}
+          onChange={handleDate}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
+      <Button variant="contained"
+        size="large"
+        color="error"
+        onClick={() =>console.log(data)}>
+        Search
+      </Button>
+      </Box>
+  );
+}
