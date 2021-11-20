c1eb05766309f17079dc48b7f36c5f0ce6bb4a9f
Tarun3165 <rawattarun1997@gmail.com>
2021-11-20 20:56:54 +0530

    search for consignments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Search from './Components/searchConsignment/Search';
+// import Consignment from './Components/searchConsignment/Consignment';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search />
+      
     </div>
   );
 }
